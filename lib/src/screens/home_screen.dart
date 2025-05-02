a2f3d88a44e204877ec1c3725ea8f754d688ba66
이보배 <a82109@MacBook-Pro-4.local>
2025-05-02 12:52:35 +0900

    feat: edit Home & add Setting screen for routing test

--- lib/src/screens/home_screen.dart
+++ lib/src/screens/home_screen.dart
@@ -1,11 +1,23 @@
+// 임시 코드
+
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('홈 화면')),
-      body: Center(child: Text('홈 화면 내용')),
+      body: Center(
+        child: InkWell(
+          onTap: () {
+            context.go('/setting');
+          },
+          child: const Text('환경 설정 화면으로 전환'),
+        ),
+      ),
     );
   }
 }
--- lib/src/screens/setting_screen.dart
+++ lib/src/screens/setting_screen.dart
@@ -1,13 +1,23 @@
-import 'package:flutter/material.dart';
+// 임시 코드
 
-class ProfileScreen extends StatelessWidget {
-  const ProfileScreen({Key? key}) : super(key: key);
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class SettingScreen extends StatelessWidget {
+  const SettingScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("환경 설정 화면")),
-      body: Center(child: Text('환경 설정 화면 내용')),
+      body: Center(
+        child: InkWell(
+          onTap: () {
+            context.go('/');
+          },
+          child: const Text('홈으로 돌아가기'),
+        ),
+      ),
     );
   }
 }
