224019f7cbaa3d911f923a4956f054fbda08ba4c
LBB <163499519+202780@users.noreply.github.com>
2025-05-02 13:29:37 +0900

    Init: 라우터 초기 설정, lib 디렉토리 구조 설정 (#5)
    * Update issue templates
    
    * docs: PR 템플릿 추가
    
    * chore: 초기 프로젝트 설정
    
    * feat: 홈 화면 생성 및 app.dart와 main.dart 분리
    
    * refactor: App을 myApp으로 변경, const 생성자(super.key) 추가
    
    * chore: add 'go_router' package for routing
    
    * feat: add setting screen
    
    * feat: initial router setup
    
    * feat: edit Home & add Setting screen for routing test
    
    * chore: set up lib directory structure
    
    * fix: correct typo
    
    ---------
    
    Co-authored-by: Binny <yeongbeen0x00@gmail.com>
    Co-authored-by: 이보배 <a82109@MacBook-Pro-4.local>
    Co-authored-by: 이보배 <a82109@Mac.kornet>

---
+++ lib/app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import '../src/router/router.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      title: 'DAWN',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      routerConfig: AppRouter.router,
+    );
+  }
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,8 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:dawn_frontend/app.dart';
 
-import 'package:dawn_frontend/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
