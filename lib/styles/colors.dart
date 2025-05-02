f720c9b675293b04263c9452104231afd444fc3d
LBB <163499519+202780@users.noreply.github.com>
2025-05-02 18:24:47 +0900

    Chore: design system setting (#7)
    * feat: add custom fonts (Noto Serif, Poppins, Source Code Pro) and register in pubspec.yaml
    
    - Added Noto Serif Condensed, Poppins, Source Code Pro font files to assets/fonts/
    - Defined each font family with corresponding weights in pubspec.yaml
    - Enables consistent typography styling across the app
    
    * feat: add typography.dart for app text styles
    
    * feat: add colors.dart for design system
    
    * chore: add background images to assets/images
    
    * chore: add UI icons
    
    ---------
    
    Co-authored-by: 이보배 <a82109@MacBook-Pro-4.local>

---
+++ lib/styles/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color darkGray = Color.fromRGBO(131, 131, 135, 1);
+  static const Color lightGray = Color.fromRGBO(239, 238, 237, 1);
+  static const Color whiteTransparent = Color.fromARGB(102, 255, 255, 255);
+}
---
+++ lib/styles/typography.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class AppTextStyle {
+  static const TextStyle heading1 = TextStyle(
+    fontSize: 32,
+    fontWeight: FontWeight.w700,
+    fontFamily: 'Noto Serif',
+    color: Color(0xFF333333),
+  );
+
+  static const TextStyle heading2 = TextStyle(
+    fontSize: 26,
+    fontWeight: FontWeight.w600,
+    fontFamily: 'Noto Serif',
+    color: Color(0xFF333333),
+  );
+
+  static const TextStyle bodyText = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'Noto Serif',
+    color: Color(0xFF333333),
+  );
+
+  static const TextStyle bodyTextPoppins = TextStyle(
+    fontSize: 13,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'Poppins',
+    color: Color(0xFF333333),
+  );
+
+  static const TextStyle tagText = TextStyle(
+    fontSize: 10,
+    fontWeight: FontWeight.w300,
+    fontFamily: 'Source Code Pro',
+    color: Color(0xFF333333),
+  );
+}
