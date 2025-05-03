aae706b06cd05f133dd2f4307b2eb18365cea977
이보배 <a82109@MacBook-Pro-4.local>
2025-05-03 21:29:43 +0900

    refactor: remove hardcoding by editing route paths into constants

--- lib/src/core/router/router.dart
+++ lib/src/core/router/router.dart
@@ -3,15 +3,23 @@ import 'package:go_router/go_router.dart';
 import '../../presentation/views/home/home.dart';
 import '../../presentation/views/setting/setting.dart';
 
+class AppRoutes {
+  static const String home = '/';
+  static const String setting = '/setting';
+}
+
 class AppRouter {
   static final GoRouter router = GoRouter(
-    initialLocation: '/',
+    initialLocation: AppRoutes.home,
     routes: [
       // 홈 화면
-      GoRoute(path: '/', builder: (context, state) => const HomeScreen()),
+      GoRoute(
+        path: AppRoutes.home,
+        builder: (context, state) => const HomeScreen(),
+      ),
       // 환경 설정 화면
       GoRoute(
-        path: '/setting',
+        path: AppRoutes.setting,
         builder: (context, state) => const SettingScreen(),
       ),
     ],
