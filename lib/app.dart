a0cd98dc6b65e3ba50fdd1a4219da1354d3dd59c
yb0x00 <yeongbeen0x00@gmail.com>
2025-05-03 20:49:39 +0900

    chore: organize project structure with MVVM layers

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import '../src/router/router.dart';
+import 'src/core/router/router.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
