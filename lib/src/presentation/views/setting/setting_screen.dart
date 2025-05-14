c22c19ff671646c010d51f3e1fbfebc990cb410e
yb0x00 <yeongbeen0x00@gmail.com>
2025-05-14 17:35:11 +0900

    feat: add thank-you message to settings screen

--- lib/src/presentation/views/setting/setting_screen.dart
+++ lib/src/presentation/views/setting/setting_screen.dart
@@ -1,22 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:dawn_frontend/src/presentation/widgets/custom_scaffold.dart';
 import 'package:dawn_frontend/src/presentation/widgets/custom_bottom_app_bar.dart';
+
+import '../../../../l10n/app_localizations.dart';
+import '../../../core/theme/typography.dart';
+import '../../../core/utils/constants/constants.dart';
 
 class SettingScreen extends StatelessWidget {
   const SettingScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final local = AppLocalizations.of(context)!;
+
     return CustomScaffold(
       body: Center(
-        child: Center(
-          child: const Text(
-            '환경 설정 화면입니다.',
-            style: TextStyle(color: Colors.white, fontSize: 24),
+        child: Padding(
+          padding: const EdgeInsets.all(24),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                local.settingsIntroMessage,
+                textAlign: TextAlign.center,
+                style: AppTextStyle.bodyText.copyWith(
+                  fontSize: 15,
+                  color: Colors.white70,
+                ),
+              ),
+              const SizedBox(height: 24),
+
+              GestureDetector(
+                onTap: () {
+                  Clipboard.setData(
+                    const ClipboardData(text: Strings.teamEmail),
+                  );
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text(local.emailCopied),
+                      duration: const Duration(seconds: 2),
+                    ),
+                  );
+                },
+                child: Text(
+                  Strings.teamEmail,
+                  style: const TextStyle(
+                    color: Color(0xFF816161),
+                    fontSize: 16,
+                    decoration: TextDecoration.underline,
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
-      bottomNavigationBar: CustomBottomAppBar(),
+      bottomNavigationBar: const CustomBottomAppBar(),
     );
   }
 }
